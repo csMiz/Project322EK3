27cfee449d7bd6198905edf770e83e335628cb47
csMiz <38314406+csMiz@users.noreply.github.com>
2024-12-17 20:07:18 +0800

    note do pass

--- MizParrot/Form1.vb
+++ MizParrot/Form1.vb
@@ -1,7 +1,9 @@
-﻿Public Class Form1
+﻿Imports System.ComponentModel
+
+Public Class Form1
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
-
+        InitializeXAudio2()
 
         Form1_Resize(Nothing, Nothing)
     End Sub
@@ -9,6 +11,14 @@
     Private Sub Form1_Resize(sender As Object, e As EventArgs) Handles Me.Resize
         Label1.Location = New Point(6, 6)
         Label1.Size = Me.ClientSize - New Size(12, 12)
+    End Sub
+
+    Private Sub Form1_Click(sender As Object, e As EventArgs) Handles Me.Click, Label1.Click
+        Play()
+    End Sub
+
+    Private Sub Form1_Closing(sender As Object, e As CancelEventArgs) Handles Me.Closing
+        DisposeXAudio2()
     End Sub
 
 End Class
--- MizParrot/ParrotSoundManager.vb
+++ MizParrot/ParrotSoundManager.vb
@@ -1,24 +1,60 @@
-﻿Imports SharpDX.Multimedia
+﻿Imports System.Runtime.InteropServices
+Imports SharpDX.Multimedia
 Imports SharpDX.XAudio2
 
 Module ParrotSoundManager
 
+    Public XAudio2Context As XAudio2 = Nothing
+    Public XAMasteringVoice As MasteringVoice = Nothing
+    Public XASourceVoice As SourceVoice = Nothing
+
+    Public do_data(44100 * 2 - 1) As Int16
+
     Public Sub InitializeXAudio2()
-        Dim xa As XAudio2 = New XAudio2()
-        Dim mv As MasteringVoice = New MasteringVoice(xa)
-        Dim wformat As New WaveFormat
-        With wformat
-            ' TODO:
-        End With
-        Dim sv As SourceVoice = New SourceVoice(xa, wformat)
+        XAudio2Context = New XAudio2()
+        XAMasteringVoice = New MasteringVoice(XAudio2Context, 2, 44100)
+        Dim wformat As New WaveFormat(44100, 16, 2)  ' int16, 44100HZ, 2chan
+        XASourceVoice = New SourceVoice(XAudio2Context, wformat)
+
+        For i = 0 To 44100 - 1
+            Dim t As Single = i / 44100.0
+            Dim val As Single = Math.Sin(Math.PI * 2.0 * 261.63 * t)
+            do_data(i * 2 + 0) = CInt(val * 32767)
+            do_data(i * 2 + 1) = do_data(i * 2 + 0)
+        Next
+    End Sub
+
+    Public Sub Play()
+        Dim dstream As New SharpDX.DataStream(44100 * 4, True, True)
+        dstream.Position = 0
+        For i = 0 To 44100 * 2 - 1
+            dstream.Write(do_data(i))
+        Next
 
         Dim data_buffer As AudioBuffer = New AudioBuffer()
         With data_buffer
-            ' TODO:
+            .AudioBytes = 44100 * 4 * 1
+            .AudioDataPointer = dstream.DataPointer
+            .Context = Nothing
+            .Flags = BufferFlags.EndOfStream
+            .LoopCount = 0
+            .LoopBegin = 0
+            .LoopCount = 0
+            .PlayBegin = 0
+            .PlayLength = 0
         End With
 
-        sv.SubmitSourceBuffer(data_buffer, Nothing)
-        sv.Start(0)
+        XASourceVoice.SubmitSourceBuffer(data_buffer, Nothing)
+        XASourceVoice.Start(0)
+
+
+    End Sub
+
+    Public Sub DisposeXAudio2()
+
+        XAudio2Context.Dispose()
+        XAMasteringVoice.Dispose()
+        XASourceVoice.Dispose()
     End Sub
 
 End Module
