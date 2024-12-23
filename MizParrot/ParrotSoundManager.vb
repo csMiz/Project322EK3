f96c301f02a606d8ba778a36e0a59e4ae85855d8
csMiz <38314406+csMiz@users.noreply.github.com>
2024-12-23 23:19:42 +0800

    adsr time

--- MizParrot/ParrotSoundManager.vb
+++ MizParrot/ParrotSoundManager.vb
@@ -10,9 +10,10 @@ Module ParrotSoundManager
     'Public XASourceVoice As SourceVoice = Nothing
     Public SourceVoiceChannel As New List(Of SourceVoice)
     Public SourceVoiceStatus As New List(Of Integer)
+    Public SourceVoicePitch As New List(Of Integer)
     Public StatusMutex As New Object
 
-    Public NoteBuffer As New Dictionary(Of Integer, SharpDX.DataStream)
+    Public NoteBuffer40 As SharpDX.DataStream
 
     Public Sub InitializeXAudio2()
         XAudio2Context = New XAudio2()
@@ -21,52 +22,26 @@ Module ParrotSoundManager
         'XASourceVoice = New SourceVoice(XAudio2Context, wformat)
         For i = 0 To 36 - 1
             Dim chan_id As Integer = i
-            Dim sv As New SourceVoice(XAudio2Context, wformat, VoiceFlags.None, 4.0, True)
+            Dim sv As New SourceVoice(XAudio2Context, wformat, VoiceFlags.None, 3.0, True)
             AddHandler sv.BufferEnd, Sub()
-                                         SyncLock StatusMutex
-                                             SourceVoiceStatus(chan_id) = 0
-                                         End SyncLock
+                                         DeallocateChannel(chan_id)
                                      End Sub
             SourceVoiceChannel.Add(sv)
             SyncLock StatusMutex
                 SourceVoiceStatus.Add(0)
+                SourceVoicePitch.Add(-1)
             End SyncLock
         Next
 
-        For i = 10 To 70
-            GenerateNoteBuffer(i)
-        Next
-
-    End Sub
-
-    Public Sub PlayNote(pitch As Integer)
-        Dim data_buffer As AudioBuffer = New AudioBuffer()
-        With data_buffer
-            .AudioBytes = 44100 * 4 * 1
-            .AudioDataPointer = NoteBuffer(pitch).DataPointer
-            .Context = Nothing
-            .Flags = BufferFlags.EndOfStream
-            .LoopCount = 0
-            .LoopBegin = 0
-            .LoopCount = 0
-            .PlayBegin = 0
-            .PlayLength = 0
-        End With
-
-        Dim chan_id As Integer = AllocateChannel()
-        If chan_id >= 0 Then
-            Dim sv As SourceVoice = SourceVoiceChannel(chan_id)
-            sv.SubmitSourceBuffer(data_buffer, Nothing)
-            sv.Start(0)
-        End If
+        GenerateNoteBuffer()
 
     End Sub
 
     Public Sub PlayNote2(pitch As Integer)
         Dim data_buffer As AudioBuffer = New AudioBuffer()
         With data_buffer
-            .AudioBytes = 44100 * 4 * 1
-            .AudioDataPointer = NoteBuffer(40).DataPointer
+            .AudioBytes = 44100 * 4 * 3
+            .AudioDataPointer = NoteBuffer40.DataPointer
             .Context = Nothing
             .Flags = BufferFlags.EndOfStream
             .LoopCount = 0
@@ -79,6 +54,9 @@ Module ParrotSoundManager
         Dim chan_id As Integer = AllocateChannel()
         If chan_id >= 0 Then
             Dim sv As SourceVoice = SourceVoiceChannel(chan_id)
+            SyncLock StatusMutex
+                SourceVoicePitch(chan_id) = pitch
+            End SyncLock
             sv.SubmitSourceBuffer(data_buffer, Nothing)
             sv.SetFrequencyRatio(2.0 ^ ((pitch - 40) / 12.0))
             sv.Start(0)
@@ -87,17 +65,13 @@ Module ParrotSoundManager
     End Sub
 
     Public Sub DisposeXAudio2()
-        For Each pitch In NoteBuffer.Values
-            pitch.Dispose()
-        Next
-        NoteBuffer.Clear()
-
+        NoteBuffer40.Dispose()
         For Each sv In SourceVoiceChannel
             sv.Dispose()
         Next
         SourceVoiceChannel.Clear()
         SourceVoiceStatus.Clear()
-        'XASourceVoice.Dispose()
+        SourceVoicePitch.Clear()
 
         XAMasteringVoice.Dispose()
 
@@ -105,12 +79,12 @@ Module ParrotSoundManager
 
     End Sub
 
-    Public Sub GenerateNoteBuffer(pitch As Integer)
+    Public Sub GenerateNoteBuffer(Optional pitch As Integer = 40)
         Dim freq As Single = 440.0 * (2.0 ^ ((pitch - 49) / 12.0))
 
-        Dim dstream As New SharpDX.DataStream(44100 * 4 * 1, True, True)
+        Dim dstream As New SharpDX.DataStream(44100 * 4 * 3, True, True)
         dstream.Position = 0
-        For i = 0 To 44100 - 1
+        For i = 0 To 44100 * 3 - 1
             Dim t As Single = i / 44100.0
             Dim val As Single = Math.Sin(Math.PI * 2.0 * freq * t)
 
@@ -118,7 +92,7 @@ Module ParrotSoundManager
             dstream.Write(CShort(val * 32767))
         Next
 
-        NoteBuffer.Add(pitch, dstream)
+        NoteBuffer40 = dstream
     End Sub
 
     Public Function AllocateChannel() As Integer
@@ -127,6 +101,7 @@ Module ParrotSoundManager
                 Dim status As Integer = SourceVoiceStatus(i)
                 If status = 0 Then
                     SourceVoiceStatus(i) = 1
+                    SourceVoicePitch(i) = -1
                     Return i
                 End If
             Next
@@ -136,7 +111,10 @@ Module ParrotSoundManager
 
     Public Sub DeallocateChannel(channel_id As Integer)
         SyncLock StatusMutex
+            SourceVoiceChannel(channel_id).Stop()
+            SourceVoiceChannel(channel_id).FlushSourceBuffers()
             SourceVoiceStatus(channel_id) = 0
+            SourceVoicePitch(channel_id) = -1
         End SyncLock
     End Sub
 
@@ -153,19 +131,22 @@ Module ParrotSoundManager
             SyncLock StatusMutex
                 For i = 0 To SourceVoiceStatus.Count - 1
                     If SourceVoiceStatus(i) = 1 Then
+                        Dim pitch_speed_rate As Single = 2.0 ^ ((SourceVoicePitch(i) - 40) / 12.0)
                         Dim vol As Single = 0.0
                         SourceVoiceChannel(i).GetVolume(vol)
-                        vol += 0.2
+                        vol += 0.2 * pitch_speed_rate
                         If vol >= 1.0 Then
                             SourceVoiceStatus(i) = 2
                         End If
                         SourceVoiceChannel(i).SetVolume(Math.Min(vol, 1.0))
                     ElseIf SourceVoiceStatus(i) = 2 Then
+                        Dim pitch_speed_rate As Single = 2.0 ^ ((SourceVoicePitch(i) - 40) / 12.0)
                         Dim vol As Single = 0.0
                         SourceVoiceChannel(i).GetVolume(vol)
-                        vol -= 0.03
+                        vol -= 0.03 * pitch_speed_rate
                         If vol <= 0.0 Then
                             vol = 0.0
+                            DeallocateChannel(i)
                         End If
                         SourceVoiceChannel(i).SetVolume(vol)
                     End If
